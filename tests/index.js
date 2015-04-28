2da14e45927044c5e9a526fa2eee302ec0ff22a3
Adam Loving <adamloving@gmail.com>
2015-04-28 11:20:37 -0700

    work in progress on tests

--- tests/index.js
+++ tests/index.js
@@ -18,28 +18,26 @@ describe('ConfigSection', function() {
         b.addInt('vnodeid', 0);
         b.addBranch('data');
         
-        // var d = b.addBranch('data2');
-        // d = d.addBranch('data_inside');
-        // d.addAttrText('attr', 'a');
-        // d.addAttrInt('attr2', 5);
-        // d.addAttrInt64('attr3', 5);
-        // d.addAttrFloat('attr4', 5.5);
+        var d = b.addBranch('data2');
+        d = d.addBranch('data_inside');
+        d.addAttrText('attr', 'a');
+        d.addAttrInt('attr2', 5);
+        d.addAttrInt64('attr3', 5);
+        d.addAttrFloat('attr4', 5.5);
         
-        // d.addText('foo', 'bar');
-        // d.addText('foo2', 'bar\nbar');
-        // d.addText('foo3', 'bar\nbar2\n');
-        // d.addTimestamp('ts', 'tbd');
-        //     //int(calendar.timegm(
-        //     //    dateutil.parser.parse(' '.join('2010-07-30 00:19:31 +0200 CEST'
-        //     // .split(' ')[0:-1])).timetuple())))
-        // d.addInt('quu', 42);
-        // d.addInt64('quu64', 42);
-        // d.addFloat('quu12', 42.7);
-        // d.addBool('quu2', true);
-        // d.addRaw('quu2', 'aAZERTYIOIUTRDCVGHGVG\ngsjgfhdshdFhjs\n');
+        d.addText('foo', 'bar');
+        d.addText('foo2', 'bar\nbar');
+        d.addText('foo3', 'bar\nbar2\n');
+        d.addTimestamp('ts', new Date('2010-07-30T00:19:31Z').getTime());
+        d.addInt('quu', 42);
+        d.addInt64('quu64', 42);
+        d.addFloat('quu12', 42.7);
+        d.addBool('quu2', true);
+        d.addRaw('quu2', 'aAZERTYIOIUTRDCVGHGVG\ngsjgfhdshdFhjs\n');
     });
 
     describe('branch', function() {
+        
         it('should return simple data', function() {
             assert(cs.getBranch('cmd_desc').getValInt('id') === 1234);
             assert(cs.getBranch('cmd_desc').getValString('name') === 'STATUS');
@@ -56,9 +54,14 @@ describe('ConfigSection', function() {
         });
     });
 
-    describe.skip('binary', function() {
-        it('should convert to binary and back', function() {
-
+    // really this is a string format
+    describe('binary', function() {
+        it.skip('should convert to binary and back', function() {
+            var s = cs.getBinary();
+            console.log('---output---\n');
+            console.log(s);
+            console.log('---end---');
+            assert(true);
         });
     });
 });
