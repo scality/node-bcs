2f0278386716a636c97ceb66dd555862b2873c76
Adam Loving <adamloving@gmail.com>
2015-04-30 09:06:34 -0700

    WIP: (work in progress) parsing attributes and values

--- src/parser.js
+++ src/parser.js
@@ -55,15 +55,17 @@ Parser.prototype.readLine = function(line) {
             this.context = this.context.parent;
             return; // ignored
         case 'V': // value
-            // this.parseValue(restOfLine);
+            this.parseValue(restOfLine);
             return;
         case 'A':  // attribute
-            // this.parseAttr(restOfLine);
+            this.parseAttr(restOfLine);
             return;
         case undefined: // EOF
             return;
         case '\n': 
             // note: if it starts with \n, it is a continuation of a text node
+            // extra line with \n terminates text node
+            // (can also find length from size)
             // append to context
             return;
         default:
@@ -98,4 +100,98 @@ Parser.prototype.parseBranch = function(line) {
     }
 
     this.context = this.context.addBranch(name);
+};
+
+// todo: text may span multiple lines
+// this just parses the length of the value, and the first
+// chunk of the value up to the end of the first line
+Parser.prototype.parseTextOrRaw = function(line) {
+    var length = parseInt(line.substring(0, 12), 10);
+    var data = line.substring(13);
+    
+    return {
+        length: length,
+        data: data
+    };
+};
+
+Parser.prototype.parseAttr = function(line) {
+
+    if (!this.cs) {
+        throw new ConfigSectionException("Attribute without root");
+    }
+
+    var name = this.parseName(line);
+    var index = 4 + name.length;
+    var typeIndicator = line[index];
+    index += 1;
+    line = line.substing(index);
+
+    switch (typeIndicator) {
+        case 'I': // integer
+            var value = parseInt(line, 10);
+            this.cs.addAttrInt(name, value);
+            return;
+        case 'L': // long
+            value = parseInt(line);
+            this.cs.addAttrInt64(name, value);
+            return;
+        // this was commented out in the python code
+        // case 'B':
+        //   value = itob(int(line[curindex:]))
+        //   cs.addAttrBool(name, value)
+        case 'T': // text
+            this.cs.addAttrText(name, this.parseText(line));
+            return;
+        case 'F': // float
+            value = parseFloat(line);
+            this.cs.addAttrFloat(name, value);
+            return;
+    }
+};
+
+Parser.prototype.parseValue = function(line) {
+    
+    if (!this.cs) {
+        throw new ConfigSectionException("value without root");
+    }
+
+    var name = this.parseName(line);
+    var index = 4 + name.length;
+    var typeIndicator = line[index];    
+    index += 1;
+    line = line.substing(index);
+
+    switch (typeIndicator) {
+        case 'I': // integer
+            this.cs.addInt(name, parseInt(line, 10));
+            break;
+        case 'F':
+            this.cs.addFloat(name, parseFloat(line, 10));
+            break;
+        case 'L':
+            this.cs.addInt64(name, parseInt(line, 10));
+            break;
+        case 'B':
+            this.cs.addBool(name, 
+                parseInt(line.substing(index)) === 0 ? false : true);
+            break;
+        case 'T': // text
+            this.cs.addText(name, this.parseText(line));
+            break;
+        case 'R': // raw
+            var results = this.parseTextOrRaw(line);
+            this.cs.addRaw(name, results.data);
+            break;
+        case 'S': // timestamp
+            // v = int(line[curindex:])
+            // if v > time.time() * 2:
+            //     v = -1
+            // cs.addTimestamp(name, v)
+            break;
+        default: 
+            // todo: include line number, or complete line
+            throw new ConfigSectionException(
+                "BCS value type not recognized (" + typeIndicator + ")");
+    }
 };
--- tests/parser.js
+++ tests/parser.js
@@ -14,38 +14,62 @@ describe('Parser', function() {
         parser = new Parser();
     });
 
-    it('parses a basic stream', function(done) {
-        var stream = new Stream();
-        
-        parser.parse(stream, function(err, cs) {
-            if (err) {
-                throw err;
-            }
-            expect(cs).to.be.an.instanceof(ConfigSection);
-            expect(cs.name).to.equal('root');
-            expect(cs.objectList[0].name).to.equal('branch');
-            done();
+    describe.skip('streaming', function() {
+
+        it('should parse a stream object', function(done) {
+            var stream = new Stream();
+            
+            parser.parse(stream, function(err, cs) {
+                if (err) {
+                    throw err;
+                }
+                expect(cs).to.be.an.instanceof(ConfigSection);
+                expect(cs.name).to.equal('root');
+                expect(cs.objectList[0].name).to.equal('branch');
+                done();
+            });
+
+            stream.emit('data', 'S0004root\n');
+            stream.emit('data', 'B0006branch\n');
+            stream.emit('data', 'b\n');
+            stream.emit('data', 's\n');
+            stream.emit('end');
+            stream.emit('close');
         });
 
-        stream.emit('data', 'S0004root\n');
-        stream.emit('data', 'B0006branch\n');
-        stream.emit('data', 'b\n');
-        stream.emit('data', 's\n');
-        stream.emit('end');
-        stream.emit('close');
-    });
+        it('should parse a file stream', function(done) {
+            var readStream = fs.createReadStream(__dirname + '/expected-results.txt');
+            
+            // could also have parser trigger stream events, but let's start with a callback
+            parser.parse(readStream, function(err, cs) {
+                if (err) {
+                    throw err;
+                }
+                expect(cs).to.be.an.instanceof(ConfigSection);
+                done();
+            });
 
-    it.skip('parses file stream', function(done) {
-        var readStream = fs.createReadStream(__dirname + '/expected-results.txt');
-        
-        // could also have parser trigger stream events, but let's start with a callback
-        parser.parse(readStream, function(err, cs) {
-            if (err) {
-                throw err;
-            }
-            expect(cs).to.be.an.instanceof(ConfigSection);
-            done();
         });
 
     });
+
+    describe('line parsing', function() {
+
+        var line = 'V0005quu64L42';
+
+        it('should parse the name', function() {
+            line = line.substring(1);
+            var name = parser.parseName(line);
+            expect(name).to.equal('quu64');
+        });
+
+        it('should parse Int64 value', function() {
+            line = line.substring(1);
+            parser.parseValue(line);
+            // either stub call to addInt64, or getDict and check the value   
+        });
+
+
+    });
+
 });
