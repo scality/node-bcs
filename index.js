68740bb5333a26e085d2e1a29c2c9336258e7454
Giorgio Regni <giorgio.regni@scality.com>
2015-05-04 15:12:05 -0700

    needed some more objects exported

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+ConfigSectionBranch = require('./src/branch.js');
+
 module.exports = {
-  ConfigSection: require('./src/config_section.js'),
+    ConfigSection: require('./src/config_section.js'),
+    makeBranch: function (name) {
+        return new ConfigSectionBranch(name);
+    },
+    Parser: require('./src/parser.js'),
 }
